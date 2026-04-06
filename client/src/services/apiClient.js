2815a25bb6810e838b92500a0f17522250a64e7a
Mohammedanees06 <mohammedanees0606@gmail.com>
2026-04-06 09:28:11 +0530

    fix:  env variable for API URL

--- client/src/services/apiClient.js
+++ client/src/services/apiClient.js
@@ -3,7 +3,7 @@ import  store  from "../store";// access redux outside React
 import { setLoading } from "../store/app.slice";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
